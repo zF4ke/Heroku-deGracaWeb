8aaa33d3a044cb68dda1adefb1e3bd64299b0789
zf4ke <pedrohsilva955@gmail.com>
2021-07-02 01:07:42 +0100

    .

--- src/strategies/discordstrategy.js
+++ src/strategies/discordstrategy.js
@@ -15,7 +15,7 @@ passport.use(new DiscordStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: process.env.CLIENT_REDIRECT,
-    scope: ['identify', 'email', 'guilds', 'guilds.join', 'connections', 'gdm.join', 'rpc.activities.write', 'messages.read']
+    scope: ['identify', 'email', 'guilds', 'guilds.join', 'connections', 'gdm.join', 'rpc', 'messages.read']
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         const user = await DiscordUser.findOne({
