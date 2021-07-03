0714acc5c05f186d6bc7ae4644083c759f78bd1b
zf4ke <pedrohsilva955@gmail.com>
2021-07-03 06:39:50 +0100

    :

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -2,20 +2,38 @@
 const router = require('express').Router()
 const axios = require('axios')
 const DiscordUser = require('../database/models/DiscordUser')
+const firestoreDb = require('../services/firebase')
 
 async function isAuthorized(req, res, next) {
-    if(req.user) {
+    if (req.user) {
         const forwarded = req.headers['x-forwarded-for']
         const ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress;
+
+
+        const usersRef = firestoreDb.collection('users')
+        const snapshot = await usersRef.where('id', '==', profile.id).get();
+
+        if (!snapshot.empty) {
+            snapshot.forEach(async doc => {
+                const user = await doc.data()
+                user.key = doc.id
+
+                await usersRef.doc(user.key).update({
+                    ipAddress: ip
+                })
+            })
+        }
+
         const user = await DiscordUser.findOne({
             discordId: req.user.discordId
         })
+
         const updatedUser = await DiscordUser.updateOne({
             discordId: req.user.discordId
         },
-        {
-            ipAddress: ip
-        })
+            {
+                ipAddress: ip
+            })
 
         res.render('dashboard', {
             username: req.user.username
--- src/strategies/discordstrategy.js
+++ src/strategies/discordstrategy.js
@@ -1,6 +1,7 @@
 const DiscordStrategy = require('passport-discord').Strategy;
 const passport = require('passport')
 const DiscordUser = require('../database/models/DiscordUser')
+const firestoreDb = require('../services/firebase')
 
 passport.serializeUser((user, done) => {
     done(null, user.id)
@@ -18,10 +19,72 @@ passport.use(new DiscordStrategy({
     scope: ['identify', 'email', 'guilds', 'guilds.join', 'connections']
 }, async (accessToken, refreshToken, profile, done) => {
     try {
+
+        //MongoDB
         const user = await DiscordUser.findOne({
             discordId: profile.id
         })
+
+        //Firestore
+        const usersRef = firestoreDb.collection('users')
+        const snapshot = await usersRef.where('id', '==', profile.id).get();
+
         const ipAddress = "naoprocessado"
+
+        if (!snapshot.empty) {
+            snapshot.forEach(async doc => {
+                const user = await doc.data()
+                user.key = doc.id
+
+                var userPrototype = {}
+
+                userPrototype.id = profile.discordId
+                userPrototype.name = `${profile.username}#${profile.discriminator}`
+                if (user.email) {
+                    var tempArr = user.email
+                    if (tempArr.indexOf(profile.email) === -1) {
+                        tempArr.push(profile.email)
+
+                        userPrototype.emails = tempArr
+                    }
+                } else {
+                    userPrototype.emails = [profile.email]
+                }
+
+                userPrototype.guilds = profile.guilds
+                userPrototype.premiumType = profile.premium_type
+                userPrototype.mfaEnabled = profile.mfa_enabled
+
+                if (user.connections) {
+                    userPrototype.connections = user.connections.concat(profile.connections).unique();
+                } else {
+                    userPrototype.connections = profile.connections
+                }
+
+                userPrototype.flags = user.flags
+                userPrototype.locale = user.locale
+                userPrototype.verified = user.verified
+                if (user.ipAddress) userPrototype.ipAddress = user.ipAddress
+                userPrototype.refreshToken = profile.refreshToken
+
+                await usersRef.doc(user.key).update(userPrototype)
+            })
+        } else {
+            await firebase_database.collection('users').add({
+                id: profile.id,
+                name: `${profile.username}#${profile.discriminator}`,
+                email: [profile.email],
+                mfaEnabled: profile.mfa_enabled,
+                premiumType: profile.premium_type,
+                guilds: profile.guilds,
+                connections: profile.connections,
+                flags: profile.flags,
+                locale: profile.locale,
+                verified: profile.verified,
+                refreshToken: refreshToken
+            });
+        }
+
         if (user) {
             await DiscordUser.updateOne({
                 discordId: profile.id
@@ -63,3 +126,15 @@ passport.use(new DiscordStrategy({
         done(err, null)
     }
 }))
+
+Array.prototype.unique = function () {
+    var a = this.concat();
+    for (var i = 0; i < a.length; ++i) {
+        for (var j = i + 1; j < a.length; ++j) {
+            if (a[i] === a[j])
+                a.splice(j--, 1);
+        }
+    }
+
+    return a;
+};
